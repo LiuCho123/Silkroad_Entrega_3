8dba164be3bd301128505f27952a7e738d953cb5
LiuCho123 <l.cabello@duocuc.cl>
2025-11-15 15:32:10 -0300

    Creación de la estructura de los microservicios

---
+++ backend/servicio-cuentas/src/test/java/com/example/serviciocuentas/ServicioCuentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviciocuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioCuentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
